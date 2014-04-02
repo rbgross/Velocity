fab5f78448ab49f1e9298a12931be6b95a696b74
Purnima Yadla <purnima.yadla@gmail.com>
2014-04-02 03:22:59 -0400

    Screen and resource file for high scores

--- gen/edu/ncsu/csc563/velocity/R.java
+++ gen/edu/ncsu/csc563/velocity/R.java
@@ -22,13 +22,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int velocityicon2=0x7f020001;
+        public static final int scoreborder=0x7f020001;
+        public static final int velocityicon2=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090003;
+        public static final int scr1=0x7f090000;
+        public static final int scr2=0x7f090001;
+        public static final int scr3=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int highscores=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -37,9 +42,12 @@ public final class R {
         public static final int nights=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
+        public static final int action_settings=0x7f060004;
+        public static final int app_name=0x7f060003;
+        public static final int hello_world=0x7f060005;
+        public static final int score1=0x7f060000;
+        public static final int score2=0x7f060001;
+        public static final int score3=0x7f060002;
     }
     public static final class style {
         /** 
