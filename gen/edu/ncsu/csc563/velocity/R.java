a570671415d5b1e95c8b9be7ba44bae569b2c3a9
Purnima Yadla <purnima.yadla@gmail.com>
2014-04-20 16:03:39 -0400

    Changed the Font for highscores

--- gen/edu/ncsu/csc563/velocity/R.java
+++ gen/edu/ncsu/csc563/velocity/R.java
@@ -27,10 +27,11 @@ public final class R {
         public static final int velocityicon2=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int scr1=0x7f090000;
-        public static final int scr2=0x7f090001;
-        public static final int scr3=0x7f090002;
+        public static final int action_settings=0x7f090004;
+        public static final int hstitle=0x7f090000;
+        public static final int scr1=0x7f090001;
+        public static final int scr2=0x7f090002;
+        public static final int scr3=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/edu/ncsu/csc563/velocity/DisplayHighScores.java
+++ src/edu/ncsu/csc563/velocity/DisplayHighScores.java
@@ -3,18 +3,21 @@ package edu.ncsu.csc563.velocity;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
 public class DisplayHighScores extends Activity{
 
-	TextView scr1,scr2,scr3;
+	TextView scr1,scr2,scr3,hstitle;
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.highscores);
         
+        hstitle = (TextView) this.findViewById(R.id.hstitle);
         scr1 = (TextView) this.findViewById(R.id.scr1);
         scr2 = (TextView) this.findViewById(R.id.scr2);
         scr3 = (TextView) this.findViewById(R.id.scr3); 
@@ -25,6 +28,14 @@ public class DisplayHighScores extends Activity{
 		int hs2 = sharedPref.getInt("hs2", 0);
 		int hs3 = sharedPref.getInt("hs3", 0);
 		
+		hstitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+		scr1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+		scr2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+		scr3.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+        // Set the font
+        Typeface font = Typeface.createFromAsset(getAssets(), "xirod.ttf");
+        hstitle.setTypeface(font);
+        
 		scr1.setText(hs1 + "");
 		scr2.setText(hs2 + "");
 		scr3.setText(hs3 + "");
